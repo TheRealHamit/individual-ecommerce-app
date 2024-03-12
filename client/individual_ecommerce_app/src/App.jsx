edbacf175ed4932d8ffd752b3dc9b058fdb6aafb
Spencer Leyh <spencerleyh@gmail.com>
2024-03-11 19:38:00 -0700

    Initial site scaffolding.

--- client/individual_ecommerce_app/src/App.jsx
+++ client/individual_ecommerce_app/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import './App.css';
 
 function App() {
 
   return (
     <>
-      <div>
-        <p>Hello.</p>
+      <div id="main-section">
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/register" element={<h1>Register</h1>} />
+          <Route path="/products" element={<h1>Products</h1>} />
+        </Routes>
       </div>
     </>
   )
