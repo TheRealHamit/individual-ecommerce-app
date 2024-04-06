29c1aa249b93ca584f2017499adb748122f84348
Spencer Leyh <spencerleyh@gmail.com>
2024-04-06 04:43:25 -0700

    Added filter to product list.

--- client/individual_ecommerce_app/src/components/Products.jsx
+++ client/individual_ecommerce_app/src/components/Products.jsx
@@ -1,20 +1,27 @@
 import { useContext, useEffect, useState } from "react"
 import { getProducts } from "../API";
 import Item from "./Item";
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography, TextField } from "@mui/material";
 import StateContext from "./StateContext";
 
 export default function Products() {
     const { productList } = useContext(StateContext);
+    const [filter, setFilter] = useState("");
+
+    function createItems() {
+        const items = <Grid container spacing={5}>{
+            productList.filter((p) => p.name.includes(filter)).map((p, i) => {return <Item key={'Item' + i} itemInfo={p} />})
+            }</Grid>
+        return items;
+    }
 
     return (
         <Container>
             <Typography variant="h1">
                 Products
             </Typography>
-            {productList ? <Grid container spacing={5}>{
-            productList.map((p, i) => {return <Item key={'Item' + i} itemInfo={p} />})
-            }</Grid> : null}
+            <TextField sx={{margin: '10px 0px'}} id="outlined-filter" label="Filter By Name" onChange={(e) => setFilter(e.target.value)}/>
+            {productList ? createItems() : null}
         </Container>
     )
 }
